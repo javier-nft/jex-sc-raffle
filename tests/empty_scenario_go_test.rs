cc2c45de3fb9f9303250151cc6189274913e1357
Aurélien Passion <aurelien.passion@gmail.com>
2023-02-19 11:06:59 +0100

    rename empty -> raffle

--- tests/empty_scenario_go_test.rs
+++ tests/empty_scenario_go_test.rs
@@ -1,4 +1,4 @@
 #[test]
-fn empty_go() {
-    multiversx_sc_scenario::run_go("scenarios/empty.scen.json");
+fn raffle_go() {
+    multiversx_sc_scenario::run_go("scenarios/init.scen.json");
 }
--- tests/empty_scenario_rs_test.rs
+++ tests/empty_scenario_rs_test.rs
@@ -3,11 +3,14 @@ use multiversx_sc_scenario::*;
 fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
 
-    blockchain.register_contract("file:output/jex-sc-raffle.wasm", jex_sc_raffle::ContractBuilder);
+    blockchain.register_contract(
+        "file:output/jex-sc-raffle.wasm",
+        jex_sc_raffle::ContractBuilder,
+    );
     blockchain
 }
 
 #[test]
-fn empty_rs() {
-    multiversx_sc_scenario::run_rs("scenarios/empty.scen.json", world());
+fn raffle_rs() {
+    multiversx_sc_scenario::run_rs("scenarios/init.scen.json", world());
 }

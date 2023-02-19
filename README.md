d34b81c088bbd0ac94b59d74067fa87cd0a29c61
Aurélien Passion <aurelien.passion@gmail.com>
2023-02-19 16:47:49 +0100

    code it :-)

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # jex-sc-raffle
+
+## Owner endpoints
+
+startRaffle (...)
+
+  * rewards (tokens)
+  * ticket sale duration (seconds)
+  * ticket price
+  * burn rate percent
+  * fees address
+
+
+enableBuyWithNFT (...)
+
+  * collection
+  * nonce
+  * nb tickets
+
+
+selectWinners ()
+
+  * select winners
+  * send rewards to winners
+
+endRaffle ()
+
+  * clear entries
+  * send back NFTs to owners
+
+
+## Public endpoints
+
+buyTickets (...)
+
+  * nb tickets
+
+
+buyTicketWithNFT (...)
+
+  * NFT transfer
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,263 @@
 #![no_std]
 
 multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
+
+#[derive(TopEncode, TopDecode, TypeAbi, PartialEq, Eq, Clone, Copy)]
+pub enum State {
+    Started,
+    Ended,
+}
+
+const MAX_BURN_RATE_PERCENT: u32 = 100;
 
 #[multiversx_sc::contract]
 pub trait JexScRaffleContract {
     #[init]
-    fn init(&self) {}
+    fn init(&self) {
+        self.state().set(State::Ended);
+    }
+
+    // Owner endpoints
+
+    #[endpoint(startRaffle)]
+    #[only_owner]
+    #[payable("*")]
+    fn start_raffle(
+        &self,
+        ticket_sale_duration: u64,
+        ticket_buy_token_identifier: TokenIdentifier,
+        ticket_buy_token_nonce: u64,
+        ticket_price: BigUint,
+        burn_rate_percent: u32,
+        fees_address: ManagedAddress,
+    ) {
+        require!(
+            self.state().get() == State::Ended,
+            "Current raffle not ended"
+        );
+
+        let payment = self.call_value().single_esdt();
+
+        self.fees_address().set(&fees_address);
+
+        require!(
+            burn_rate_percent <= MAX_BURN_RATE_PERCENT,
+            "Invalid burn rate percent"
+        );
+
+        self.burn_rate_percent().set(burn_rate_percent);
+        if burn_rate_percent > 0u32 {
+            let roles = self
+                .blockchain()
+                .get_esdt_local_roles(&ticket_buy_token_identifier.clone());
+            require!(roles.has_role(&EsdtLocalRole::Burn), "Missing burn role");
+        }
+
+        self.prize().set(EsdtTokenPayment::new(
+            payment.token_identifier,
+            payment.token_nonce,
+            payment.amount,
+        ));
+
+        self.ticket_price().set(&ticket_price);
+
+        self.ticket_sale_end_timestamp()
+            .set(self.blockchain().get_block_timestamp() + ticket_sale_duration);
+
+        self.ticket_token()
+            .set((ticket_buy_token_identifier, ticket_buy_token_nonce));
+
+        self.state().set(State::Started);
+    }
+
+    #[endpoint(pickWinners)]
+    #[only_owner]
+    fn pick_winners(&self, nb_winners: u16) {
+        require!(
+            self.blockchain().get_block_timestamp() > self.ticket_sale_end_timestamp().get(),
+            "Still in tickets sale period"
+        );
+
+        let entries_mapper = self.entries();
+
+        require!(
+            entries_mapper.len() >= nb_winners.into(),
+            "Too many winners"
+        );
+
+        let (ticket_token_identifier, ticket_token_nonce) = self.ticket_token().get();
+        let mut tickets_token_balance = self.blockchain().get_sc_balance(
+            &EgldOrEsdtTokenIdentifier::esdt(ticket_token_identifier.clone()),
+            ticket_token_nonce,
+        );
+
+        self.do_burn(
+            &ticket_token_identifier,
+            ticket_token_nonce,
+            &mut tickets_token_balance,
+        );
+
+        if tickets_token_balance > 0 {
+            self.send().direct_esdt(
+                &self.fees_address().get(),
+                &ticket_token_identifier,
+                ticket_token_nonce,
+                &tickets_token_balance,
+            );
+        }
+
+        let prize = self.prize().get();
+        let rewards_per_ticket = prize.amount / BigUint::from(nb_winners);
+
+        let mut rnd = RandomnessSource::new();
+        for _ in 0..nb_winners {
+            let num = 1 + rnd.next_usize_in_range(0, entries_mapper.len());
+
+            let winner = entries_mapper.get(num);
+
+            if rewards_per_ticket > 0 {
+                self.send().direct_esdt(
+                    &winner,
+                    &prize.token_identifier,
+                    prize.token_nonce,
+                    &rewards_per_ticket,
+                );
+            }
+        }
+
+        self.send_leftovers_to_owner();
+    }
+
+    #[endpoint(endRaffle)]
+    #[only_owner]
+    fn end_raffle(&self) {
+        require!(self.state().get() == State::Started, "Raffle not started");
+
+        let leftover_balance = self.get_leftover_balance();
+
+        require!(leftover_balance == 0u32, "Rewards not distributed");
+
+        self.burn_rate_percent().clear();
+        self.entries().clear();
+        self.fees_address().clear();
+        self.prize().clear();
+        self.ticket_price().clear();
+        self.ticket_sale_end_timestamp().clear();
+        self.ticket_token().clear();
+
+        self.state().set(State::Ended);
+    }
+
+    // Public endpoints
+
+    #[endpoint(buyTickets)]
+    #[payable("*")]
+    fn buy_tickets(&self, nb: u32) {
+        require!(self.state().get() == State::Started, "Raffle not started");
+
+        require!(
+            self.blockchain().get_block_timestamp() <= self.ticket_sale_end_timestamp().get(),
+            "Not in tickets sale period"
+        );
+
+        let payment = self.call_value().single_esdt();
+
+        let (ticket_token_identifier, ticket_token_nonce) = self.ticket_token().get();
+        require!(
+            payment.token_identifier == ticket_token_identifier
+                && payment.token_nonce == ticket_token_nonce,
+            "Invalid token"
+        );
+
+        require!(
+            payment.amount == self.ticket_price().get() * nb,
+            "Invalid amount"
+        );
+
+        let caller = self.blockchain().get_caller();
+        let mut entries_mapper = self.entries();
+        for _ in 0..nb {
+            entries_mapper.push(&caller);
+        }
+    }
+
+    // Functions
+
+    fn do_burn(
+        &self,
+        ticket_token_identifier: &TokenIdentifier,
+        ticket_token_nonce: u64,
+        tickets_token_balance: &mut BigUint,
+    ) {
+        let burn_rate_mapper = self.burn_rate_percent();
+        let burn_rate = burn_rate_mapper.get();
+
+        if burn_rate > 0u32 {
+            let burn_amount = tickets_token_balance.clone() * burn_rate / 100u32;
+
+            *tickets_token_balance -= &burn_amount;
+
+            self.send()
+                .esdt_local_burn(ticket_token_identifier, ticket_token_nonce, &burn_amount);
+        }
+    }
+
+    fn get_leftover_balance(&self) -> BigUint {
+        let prize = self.prize().get();
+
+        let leftover_balance = self.blockchain().get_sc_balance(
+            &EgldOrEsdtTokenIdentifier::esdt(prize.token_identifier.clone()),
+            prize.token_nonce,
+        );
+
+        leftover_balance
+    }
+
+    fn send_leftovers_to_owner(&self) {
+        let leftover_balance = self.get_leftover_balance();
+
+        let prize = self.prize().get();
+        if leftover_balance > 0u32 {
+            self.send().direct_esdt(
+                &self.blockchain().get_owner_address(),
+                &prize.token_identifier,
+                prize.token_nonce,
+                &leftover_balance,
+            );
+        }
+    }
+
+    // Storage & Views
+
+    #[view(getBurnRatePercent)]
+    #[storage_mapper("burn_rate_percent")]
+    fn burn_rate_percent(&self) -> SingleValueMapper<u32>;
+
+    #[storage_mapper("entries")]
+    fn entries(&self) -> VecMapper<ManagedAddress>;
+
+    #[view(getFeesAddress)]
+    #[storage_mapper("fees_address")]
+    fn fees_address(&self) -> SingleValueMapper<ManagedAddress>;
+
+    #[view(getPrize)]
+    #[storage_mapper("prize")]
+    fn prize(&self) -> SingleValueMapper<EsdtTokenPayment>;
+
+    #[view(getState)]
+    #[storage_mapper("state")]
+    fn state(&self) -> SingleValueMapper<State>;
+
+    #[view(getTicketPrice)]
+    #[storage_mapper("ticket_price")]
+    fn ticket_price(&self) -> SingleValueMapper<BigUint>;
+
+    #[view(getTicketSaleEndTimestamp)]
+    #[storage_mapper("ticket_sale_end_timestamp")]
+    fn ticket_sale_end_timestamp(&self) -> SingleValueMapper<u64>;
+
+    #[view(getTicketToken)]
+    #[storage_mapper("ticket_token")]
+    fn ticket_token(&self) -> SingleValueMapper<(TokenIdentifier, u64)>;
 }
